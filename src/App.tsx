9851cda0f62a7281e14340b14a1320c03710a66a
Petros <psdeme@hotmail.com>
2022-01-15 12:36:01 +0200

    added missing files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
 import React from 'react';
 import logo from './logo.svg';
+import ListItem from './ListItem';
 import './App.css';
 
+
 function App() {
+  const [state, setState] = React.useState<{ selections: string[] }>({ selections: [] });
+  const selections = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8', 'Item 9']
+
+  function handleCheckboxChange(key: string) {
+    let sel = state.selections
+    let find = sel.indexOf(key)
+    if (find > -1) {
+      sel.splice(find, 1)
+    } else {
+      sel.push(key)
+    }
+
+    setState({
+      selections: sel,
+    })
+    console.log(state)
+  }
+
+  function checkboxItems() {
+    return (
+      <React.Fragment>
+        {selections.map((selection) => (
+          <ListItem
+            key={selection}
+            text={selection}
+            handleOnChange={() => handleCheckboxChange(selection)}
+            selected={state.selections.includes(selection)}
+          ></ListItem>
+        ))}
+      </React.Fragment>
+    )
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{width: '270px', overflowY: 'scroll', margin: 'auto'}}>
+    {checkboxItems()}
+    <p>{state.selections.toString()}</p>
+    </div>
     </div>
   );
 }
---
+++ src/ListItem.tsx
@@ -0,0 +1,51 @@
+import React, { ChangeEventHandler } from 'react'
+import styled from 'styled-components'
+
+const CheckBox = styled.input`
+  margin: 6px 10px 5.8px 3px;
+  border-radius: 2px;
+  border: solid 1px #c6c4d2;
+  width: 5%;
+float: left;
+  background: ${(porps) => (porps.checked ? '#482474' : '#fbfcff')};
+`
+const Container = styled.div`
+  margin-bottom: 0px !important;
+  margin-top: 2px;
+  &:after {
+    content: "";
+    display: table;
+    clear: both;
+  }
+`
+const Description = styled.p`
+  padding-bottom: 1px;
+  margin-top: 4px;
+  font-size: 15px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  text-align: left;
+`
+
+const ListItem: React.FC<{
+  text: string
+  handleOnChange: ChangeEventHandler
+  selected: boolean
+}> = ({ text, handleOnChange, selected }) => {
+  return (
+      <Container>
+        <CheckBox
+          type="checkbox"
+          checked={selected}
+          onChange={handleOnChange} 
+        ></CheckBox>
+        <Description className="column" style={{width: '33%',
+            float:'left'}}>
+          {text}
+        </Description>
+      </Container>
+  )
+}
+
+export default ListItem
